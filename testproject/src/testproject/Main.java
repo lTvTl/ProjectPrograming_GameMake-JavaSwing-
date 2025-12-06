623cadcc4cbfff5371fd044bcfe0ae8317366f36
lTvTl <wlstjd0124@mokpo.ac.kr>
2025-12-06 11:07:51 +0900

    comit test

--- testproject/src/testproject/Main.java
+++ testproject/src/testproject/Main.java
@@ -3,12 +3,7 @@ package testproject;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//testingProgram_github
-		//테스트입니모모모
-		//오랜만에 테스트입니다.
-		//된건가?
-		//다시 테스팅
+		//comit
 	}
 
 }
